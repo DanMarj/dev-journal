8df15ce499ddb445519ce4254f237c0a65fc1a06
DanielMarj <danielmarjerrison@gmail.com>
2024-08-05 17:59:19 -0400

    Committed the basic idea for the contributions graph. I'd like it to be prettier, but how...

--- src/app/components/main-panel/contribution-graph/contribution-graph.component.ts
+++ src/app/components/main-panel/contribution-graph/contribution-graph.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContributionGraphComponent implements OnInit {
 
-  constructor() { }
+  // Remaining months of 2024, and days per month
+  months: string[] = ["August", "September", "October", "November", "December"];
+  days: number[] = [31,30,31,30,31]; // sum is 153
+  contributionOnMonthDay: boolean[][];
+
+  constructor() {
+    this.contributionOnMonthDay = this.generateRandomContributions(this.days);
+  }
 
   ngOnInit(): void {
+  }
+
+  private generateRandomContributions(daysPerMonth: number[]): boolean[][] {
+    let ret: boolean[][] = [[],[],[],[],[]];
+    for(let i = 0; i<daysPerMonth.length; i++) {
+      for(let j = 0; j<daysPerMonth[i]; j++) {
+        const randP = Math.random();
+        if(randP<0.5) {
+          ret[i][j] = true;
+        } else {
+          ret[i][j] = false;
+        }
+      }
+    }
+    console.log("ret", ret);
+    return ret;
   }
 
 }
